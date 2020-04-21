18ad6511b6a903b0b5265342a3c96cfdb38d2cba
Esko Lähdesmäki <eskilmar@hotmail.com>
2020-04-21 09:06:34 +0300

    adding theme files

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Welcome to Kubernetes Theme Customizer
+ *
+ * @package Welcome_to_Kubernetes
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function welcome_to_kubernetes_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'welcome_to_kubernetes_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'welcome_to_kubernetes_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'welcome_to_kubernetes_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function welcome_to_kubernetes_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function welcome_to_kubernetes_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function welcome_to_kubernetes_customize_preview_js() {
+	wp_enqueue_script( 'welcome-to-kubernetes-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'welcome_to_kubernetes_customize_preview_js' );
